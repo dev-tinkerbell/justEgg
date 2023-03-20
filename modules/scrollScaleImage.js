18b5652eeaefed7ce37c122cca3d56e7db39ab3f
PTK <ptk@PTK-A170375-MB.local>
2023-03-20 13:08:50 +0900

    scroll에 기반한 percentage 숫자 변경

--- modules/scrollScaleImage.js
+++ modules/scrollScaleImage.js
@@ -1,3 +1,22 @@
 export function scrollScaleImage() {
   console.log('scrollScaleImage');
+
+  const numberElement = document.querySelector('.num i');
+  const onUpdateNumber = (number) => {
+    numberElement.textContent = number;
+  };
+
+  gsap.to('.num', {
+    scrollTrigger: {
+      trigger: '.lessLand-wrap',
+      start: 'top top',
+      end: '90% bottom',
+      scrub: 1,
+      markers: true,
+      onUpdate: ({ progress }) => {
+        const number = Math.floor(progress.toFixed(2) * 83);
+        onUpdateNumber(number);
+      },
+    },
+  });
 }
