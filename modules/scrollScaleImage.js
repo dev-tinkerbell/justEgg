5e73d5674e35a153c97ccf4eb22ddcd160fa6af6
PTK <ptk@PTK-A170375-MB.local>
2023-03-22 18:32:53 +0900

    scrollScaleImage 코드 리팩토링

--- modules/scrollScaleImage.js
+++ modules/scrollScaleImage.js
@@ -1,36 +1,54 @@
 export function scrollScaleImage() {
   gsap.defaults({ ease: 'none' });
+
+  // content 스크롤 애니메이션
+  // 스크롤 트리거 animation 생성 함수
+  const createScrollTrigger = (vars) => {
+    const properties = {
+      trigger: '.lessLand-wrap',
+      start: 'top top',
+      end: 'bottom bottom',
+      scrub: true,
+      ...vars,
+    };
+
+    ScrollTrigger.create(properties);
+  };
+
   // 스크롤에 따른 퍼센트 숫자 변경
   const numberElement = document.querySelector('.num i');
   const onUpdateNumber = (number) => {
     numberElement.textContent = number;
   };
 
-  gsap.to('.num', {
-    scrollTrigger: {
-      trigger: '.lessLand-wrap',
-      start: 'top top',
-      end: '90% bottom',
-      scrub: 1,
-      onUpdate: ({ progress }) => {
-        const number = Math.floor(progress.toFixed(2) * 83);
-        onUpdateNumber(number);
-      },
+  createScrollTrigger({
+    onUpdate: ({ progress }) => {
+      const number = Math.floor(progress.toFixed(2) * 83);
+      onUpdateNumber(number);
     },
   });
 
-  // image, box 스크롤 트리거 기본 값
-  const createScrollTrigger = (animation, obj) => {
-    let defaults = {
-      animation,
-      trigger: '.lessLand-wrap',
-      start: 'top top',
-      end: 'bottom bottom',
-      scrub: true,
-    };
+  // sub detaile 타임라인
+  const subDetailTimeline = gsap.timeline();
+  subDetailTimeline.fromTo(
+    '.sub-detail',
+    {
+      opacity: 0,
+      y: 30,
+    },
+    {
+      opacity: 1,
+      y: 0,
+    }
+  );
 
-    ScrollTrigger.create({ ...defaults, ...obj });
-  };
+  // sub detaile 스크롤 트리거 생성
+  createScrollTrigger({
+    animation: subDetailTimeline,
+    start: '9% top',
+    end: '11% top',
+    scrub: 1,
+  });
 
   // img-outWrap 타임라인
   const ImgWrapTimeline = gsap.timeline();
@@ -42,47 +60,33 @@ export function scrollScaleImage() {
   });
 
   // img-outWrap 스크롤 트리거 생성
-  createScrollTrigger(ImgWrapTimeline);
+  createScrollTrigger({ animation: ImgWrapTimeline });
 
   // img-innerWrap 타임라인
-  // ---- 같은 element를 한번에 선언 할 수 있는 방법이 없을까? (중복코드 제거)
-  const imgTimeline = gsap.timeline();
-  imgTimeline
-    .to('.img-outWrap .img-innerWrap', {
-      x: '-5%',
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      y: '10%',
-      scale: 0.92,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      y: '20%',
-      scale: 0.84,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      scale: 0.77,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      y: '30%',
-      scale: 0.7,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      y: '40%',
-      scale: 0.62,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      x: '-1%',
-      scale: 0.52,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      scale: 0.47,
-    })
-    .to('.img-outWrap .img-innerWrap', {
-      scale: 0.43,
-    });
+  const createImgTimeline = () => {
+    const imgTimeline = gsap.timeline();
+    const motions = [
+      { x: '-5%' },
+      { y: '10%', scale: 0.92 },
+      { y: '20%', scale: 0.84 },
+      { y: '20%', scale: 0.84 },
+      { scale: 0.77 },
+      { y: '30%', scale: 0.7 },
+      { y: '40%', scale: 0.62 },
+      { x: '-1%', scale: 0.52 },
+      { scale: 0.47 },
+      { scale: 0.43 },
+    ];
+
+    for (let index = 0; index < motions.length; index++) {
+      imgTimeline.to('.img-outWrap .img-innerWrap', motions[index]);
+    }
+
+    return imgTimeline;
+  };
 
   // img-innerWrap 스크롤 트리거 생성
-  createScrollTrigger(imgTimeline);
+  createScrollTrigger({ animation: createImgTimeline() });
 
   // picture 타임라인
   const pictureTimeline = gsap.timeline();
@@ -96,41 +100,35 @@ export function scrollScaleImage() {
     });
 
   // picture 스크롤 트리거 생성
-  createScrollTrigger(pictureTimeline);
+  createScrollTrigger({ animation: pictureTimeline });
 
-  // box 타임라인
-  const boxTimeline = gsap.timeline();
-  boxTimeline
-    .to('.one > .inner', {
-      x: '0%',
-    })
-    .set('.img-outWrap', {
-      zIndex: 0,
-    })
-    .to('.two > .inner', {
-      y: '0%',
-    })
-    .to('.three > .inner', {
-      x: '0%',
-    })
-    .to('.four > .inner', {
-      y: '0%',
-    })
-    .to('.five > .inner', {
-      x: '0%',
-    })
-    .to('.six > .inner', {
-      y: '0%',
-    })
-    .to('.seven > .inner', {
-      x: '0%',
-    })
-    .to('.eight > .inner', {
-      y: '0%',
-    })
-    .to('.nine > .inner', {
-      x: '0%',
+  const getBoxes = () => {
+    let currentBox = document.querySelector('.box.one');
+    let boxes = [];
+
+    while (currentBox?.children.length) {
+      boxes = [...boxes, currentBox.children[0]];
+      currentBox = currentBox.children[1];
+    }
+    return boxes;
+  };
+
+  const createBoxTimeline = () => {
+    const boxTimeline = gsap.timeline();
+    const motions = [{ x: '0%' }, { y: '0%' }];
+    const boxes = getBoxes();
+
+    boxes.forEach((box, index) => {
+      if (index === 0) {
+        boxTimeline.to(box, motions[0]).set('.img-outWrap', { zIndex: 0 });
+        return;
+      }
+
+      boxTimeline.to(box, index % 2 ? motions[1] : motions[0]);
     });
 
-  createScrollTrigger(boxTimeline, { markers: true });
+    return boxTimeline;
+  };
+
+  createScrollTrigger({ animation: createBoxTimeline() });
 }
