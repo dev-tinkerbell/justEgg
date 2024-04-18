ac64899b2c0c653c3328b6f4fd0d147b8a0d4b18
claire.kim <claire.kim@cheilpengtai.com>
2024-04-18 11:13:54 +0900

    v2 - deep link test

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const UTILS = (function () {
   return {
+    isIosDevice: (function () {
+      var isIos = /iPad|iPhone|iPod/.test(navigator.userAgent);
+      isIos ? document.documentElement.classList.add("isIosDevice") : document.documentElement.classList.add("isNotIosDevice");
+      return isIos;
+    })(),
     isTouchDevice: (function () {
       var checkDevice = "ontouchstart" in window || (window.DocumentTouch && document instanceof window.DocumentTouch);
       checkDevice ? document.documentElement.classList.add("isTouchDevice") : document.documentElement.classList.add("isNotTouchDevice");
